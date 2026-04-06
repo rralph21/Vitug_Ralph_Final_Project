b607a7a101539de9b38b8d790da7f186aab03fd9
Ralph.Vitug <rralph@rrc.ca>
2026-04-05 20:52:56 -0500

    edit: services and controller CRUD METHODS with sample apts

--- src/api/v1/controllers/aptControllers.ts
+++ src/api/v1/controllers/aptControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { HTTP_STATUS } from "../../../constant/httpConstants";
-import { getAllAptsAsync, createAptAsync, updateAptAsync, deleteAptAsync } from "../services/aptServices";
+import { getAllAptsAsync, createAptAsync, updateAptAsync, deleteAptAsync, getAptByIdAsync } from "../services/aptServices";
+
 
 export const getAllApts = async(req: Request, res: Response) => {
     const apts = await getAllAptsAsync();
@@ -8,6 +9,12 @@ export const getAllApts = async(req: Request, res: Response) => {
     // Logic to get all items
     res.status(HTTP_STATUS.OK).send(apts);
 };
+
+export const getAptById = async(req: Request, res: Response) => {
+    // Logic to get an item by ID
+    const apt = await getAptByIdAsync(req.params.id);
+    res.status(HTTP_STATUS.OK).send(apt);
+}
 
 export const createApt = async(req: Request, res: Response) => {
     // Logic to create a new item
@@ -17,12 +24,12 @@ export const createApt = async(req: Request, res: Response) => {
 
 export const updateApt = async(req: Request, res: Response) => {
     // Logic to update an item
-    // const updatedItem = await updateAptAsync(req.params.id, req.body.item);
-    // res.status(HTTP_STATUS.OK).send(updatedItem);
+    const updatedItem = await updateAptAsync(req.params.id, req.body.item);
+    res.status(HTTP_STATUS.OK).send(updatedItem);
 };
 
 export const deleteApt = async(req: Request, res: Response) => {
     // Logic to delete an item
-    // const deletedItem = await deleteAptAsync(req.params.id);
-    // res.status(HTTP_STATUS.NO_CONTENT).send(deletedItem);
+    const deletedItem = await deleteAptAsync(req.params.id);
+    res.status(HTTP_STATUS.NO_CONTENT).send(deletedItem);
 };
--- src/api/v1/services/aptServices.ts
+++ src/api/v1/services/aptServices.ts
@@ -1,19 +1,51 @@
-export const getAllAptsAsync = (): string[] => {
+import { Appointment } from "../models/aptStatus";
+import { CreateAppointmentInput } from "../models/createApt";
+import { UpdateAppointmentInput } from "../models/updateApt";
+import { sampleAppointments } from "../models/sampleApt";
+import { HTTP_STATUS } from "src/constant/httpConstants";
+
+export const getAllAptsAsync = (): Appointment[] => {
     // Logic to process all items from the database
-    return ["Item 1", "Item 2"];
+    return sampleAppointments;
 };
 
-export const createAptAsync = (item: string): string => {
+export const getAptByIdAsync = (id: number): Appointment => {
+    // Logic to get an item by ID from the database
+    const apt = sampleAppointments.find((apt) => apt.id === id);
+    if (!apt) {
+        throw new Error("Appointment not found"), HTTP_STATUS.NOT_FOUND;
+    }
+    return apt;
+};
+
+
+export const createAptAsync = (item: CreateAppointmentInput): Appointment => {
     // Logic to add a new item to the database
-    return "Item added";
+    const newApt: Appointment = {
+        ...item,
+        id: sampleAppointments.length + 1,
+        createdAt: new Date().toISOString()
+    };
+    sampleAppointments.push(newApt);
+    return newApt;
 };
 
-export const updateAptAsync = (id: number, item: string): string => {
+export const updateAptAsync = (id: number, item: UpdateAppointmentInput): Appointment => {
     // Logic to update an item in the database
-    return "Item updated";
+    const aptIndex = sampleAppointments.findIndex((apt) => apt.id === id);
+    if (aptIndex === -1) {
+        throw new Error("Appointment not found"), HTTP_STATUS.NOT_FOUND;
+    }
+    sampleAppointments[aptIndex] = { ...sampleAppointments[aptIndex], ...item };
+    return sampleAppointments[aptIndex];
 };
 
-export const deleteAptAsync = (id: number): string => {
+export const deleteAptAsync = (id: number): Appointment => {
     // Logic to delete an item from the database
-    return "Item deleted";
+    const aptIndex = sampleAppointments.findIndex((apt) => apt.id === id);
+    if (aptIndex === -1) {
+        throw new Error("Appointment not found"), HTTP_STATUS.NOT_FOUND;
+    }
+    const [deletedApt] = sampleAppointments.splice(aptIndex, 1);
+    return deletedApt;
 };
