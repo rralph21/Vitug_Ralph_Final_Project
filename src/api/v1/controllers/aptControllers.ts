160e92ae4a8b8e8981a91de7da136040da7f9d69
Ralph.Vitug <rralph@rrc.ca>
2026-04-05 20:21:57 -0500

    feat: basic async controller methods

--- src/api/v1/controllers/aptControllers.ts
+++ src/api/v1/controllers/aptControllers.ts
@@ -1,22 +1,28 @@
 import { Request, Response } from "express";
 import { HTTP_STATUS } from "../../../constant/httpConstants";
+import { getAllAptsAsync, createAptAsync, updateAptAsync, deleteAptAsync } from "../services/aptServices";
 
-export const getAllApts = (req: Request, res: Response) => {
+export const getAllApts = async(req: Request, res: Response) => {
+    const apts = await getAllAptsAsync();
+
     // Logic to get all items
-    res.status(HTTP_STATUS.OK).send("Get all items");
+    res.status(HTTP_STATUS.OK).send(apts);
 };
 
-export const createApt = (req: Request, res: Response) => {
+export const createApt = async(req: Request, res: Response) => {
     // Logic to create a new item
-    res.status(HTTP_STATUS.CREATED).send("Create a new item");
+    const newItem = await createAptAsync(req.body.item);
+    res.status(HTTP_STATUS.CREATED).send(newItem);
 };
 
-export const updateApt = (req: Request, res: Response) => {
+export const updateApt = async(req: Request, res: Response) => {
     // Logic to update an item
-    res.status(HTTP_STATUS.OK).send("Update an item");
+    // const updatedItem = await updateAptAsync(req.params.id, req.body.item);
+    // res.status(HTTP_STATUS.OK).send(updatedItem);
 };
 
-export const deleteApt = (req: Request, res: Response) => {
+export const deleteApt = async(req: Request, res: Response) => {
     // Logic to delete an item
-    res.status(HTTP_STATUS.NO_CONTENT).send("Delete an item");
+    // const deletedItem = await deleteAptAsync(req.params.id);
+    // res.status(HTTP_STATUS.NO_CONTENT).send(deletedItem);
 };
