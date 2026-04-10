09abc6a697e673f56b10a25c90f330de2860a37f
Ralph.Vitug <rralph@rrc.ca>
2026-04-10 10:43:36 -0500

    feat: adminRoutes without middleware , , , yet

--- src/api/v1/routes/adminRoutes.ts
+++ src/api/v1/routes/adminRoutes.ts
@@ -1,1 +1,11 @@
-// adminroutes.ts will be used to define all the routes related to admin operations such as creating a new admin, updating admin details, deleting an admin, etc.
+import { Router } from "node_modules/@types/express";
+import {setUserClaims} from "../controllers/adminController"
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize"
+
+const adminRoutes: Router = Router();
+
+// no roles defined to set up inital admin user
+// upate this route to be protected by admin role after setting up the initial admin user
+adminRoutes.post('/admin/setClaims', setUserClaims);
+export default adminRoutes;
