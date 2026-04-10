71ba659dc6db9e92f6a8e6935b06a766a04dfd96
Ralph.Vitug <rralph@rrc.ca>
2026-04-10 10:42:21 -0500

    feat: adminController for setting claims

--- src/api/v1/controllers/adminController.ts
+++ src/api/v1/controllers/adminController.ts
@@ -1,1 +1,14 @@
-// adminController.ts will be used to handle all the admin related operations such as creating a new admin, updating admin details, deleting an admin, etc.
+import { Request, Response } from "express";
+import {auth} from "../../../config/firebaseConfig";
+import { UserRecord } from "firebase-admin/auth";
+
+export const setUserClaims = async (req: Request, res: Response) => {
+    let userClaimsToset = req.body
+
+    const user: UserRecord = await auth.getUser(userClaimsToset.uid);
+    
+    await auth.setCustomUserClaims(userClaimsToset.uid, userClaimsToset.claims)
+
+    res.status(200).send("Ok");
+    return
+};
