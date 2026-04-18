9cf4bb2400ad693ceacc5ad72b12f1973b267aa8
Ralph.Vitug <rralph@rrc.ca>
2026-04-18 17:32:27 -0500

    feat: authentication middleware into routes

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,43 +1,10 @@
-import { Express } from "express";
 import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
 import { generateSwaggerSpec } from "./swaggerConfig";
 
-export const setupSwagger = (app: Express): void => {
-    const swaggerSpec = generateSwaggerSpec();
-
-    /**
-     * @swagger
-     * /api-docs:
-     *   get:
-     *     summary: Swagger UI
-     *     tags:
-     *       - Documentation
-     *     responses:
-     *       200:
-     *         description: Interactive Swagger UI page
-     *         content:
-     *           text/html:
-     *             schema:
-     *               type: string
-     */
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-    /**
-     * @swagger
-     * /api-docs.json:
-     *   get:
-     *     summary: OpenAPI JSON document
-     *     tags:
-     *       - Documentation
-     *     responses:
-     *       200:
-     *         description: Raw OpenAPI JSON specification
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     */
-    app.get("/api-docs.json", (_req, res) => {
-        res.json(swaggerSpec);
-    });
+const setupSwagger = (app: Express): void => {
+    const specs = generateSwaggerSpec();
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 };
+
+export default setupSwagger;
